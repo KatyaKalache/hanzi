83864b56ad62fa8c608c21ed434924e41b2ff4a5
KatyaKalache <katya@kalache.fr>
2017-10-29 00:22:04 +0000

    Added edge cases: if nothing was entered and if something beside chinese character was entered

--- dev/decompose.py
+++ dev/decompose.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
+# Decomposes character into components
+
 from sys import argv
 from pymongo import MongoClient
+import re
 
-client = MongoClient()
-db = client.dictionary
-argument = argv[1]
-for i in argument:
-  search_char = db.characters.find({"character": i})
-  for decomp in search_char:
-    elements = decomp["decomposition"]
-    print (decomp['character'])
-    print (decomp["definition"])
-    for each in elements:
-      search_radical = db.radicals.find({"radical": each})
-      for radical in search_radical:
-        print (each, end=" - ")
-        print (radical["meaning"])
+if __name__ == "__main__":
+    client = MongoClient()
+    db = client.dictionary
+    if (len(argv) < 2):
+        print ("Please enter at least one character")
+    else:
+        argument = argv[1]
+        valid = re.findall(r'[\u4e00-\u9fff]+', argument)
+        if valid == []:
+            print ("Please enter a chinese character")
+        for i in valid:
+            search_char = db.characters.find({"character": i})
+            for decomp in search_char:
+                elements = decomp["decomposition"]
+                print (decomp["character"])
+                print (decomp["definition"])
+                for each in elements:
+                    search_radical = db.radicals.find({"radical": each})
+                    for radical in search_radical:
+                        print (each, end=" - ")
+                        print (radical["meaning"])
