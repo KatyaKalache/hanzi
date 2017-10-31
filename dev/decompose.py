7fde00b11cc63158722e1f827c11e44b8db554b5
KatyaKalache <katya@kalache.fr>
2017-10-31 23:26:29 +0000

    in progress

--- dev/decompose.py
+++ dev/decompose.py
@@ -14,40 +14,40 @@ if (len(argv) < 2):
 
 def decom():
     argument = argv[1]
+    counter = 0
     valid = re.findall(r'[\u4e00-\u9fff]+', argument)
+    dictionary = {}
     if valid == []:
-        print ("Please enter a chinese character")
-    for i in valid:
-        search_char = db.characters.find({"character": i})
-        print ("This is i {}".format(i))
-    return (search_char)
+        print ("Please enter a Chinese character")
+    while (counter < 3):
+        for i in valid:
+            for j in i:
+                search_char = (db.characters.find({"character": j}))
+                for decomp in search_char:
+                    definition = (decomp["definition"])
+                    print (definition)
+        counter += 1
+#    return (search_char)
 search_char=decom()
+#search_char=decom()
+#print (search_char)
+#def translate(search_char):
+ #   for decomp in search_char:
 
-def get_word(search_char):
-    for decomp in search_char:
-        character = (decomp["character"])
-        definition = (decomp["definition"])
-        res = (character + " " + definition)
-        print (res)
-    return (res)
-res = get_word(search_char)
-search_char=decom()
 
-def translate(search_char):
-    for decomp in search_char:
-        elements = (decomp["decomposition"])
-        character = (decomp["character"])
-        definition = (decomp["definition"])
-        res = (character + " " + definition + '\n' + elements)
-        print (elements)
-    return (elements)
-elements = translate(search_char)
+  #      elements = (decomp["decomposition"])
+   #     character = (decomp["character"])
+    #    definition = (decomp["definition"])
+     #   res = (character + " " + definition + '\n' + elements)
+      #  print (elements)
+   # return (elements)
+#elements = translate(search_char)
 
-def radical(elements):
-    for each in elements:
-        search_radical = db.radicals.find({"radical": each})
-        for radical in search_radical:
-            rad_mean = (each + radical["meaning"])
-            print (rad_mean)
-    return (rad_mean)
-rad_mean = radical(elements)
+#def radical(elements):
+ #   for each in elements:
+  #      search_radical = db.radicals.find({"radical": each})
+   #     for radical in search_radical:
+    #        rad_mean = (each + radical["meaning"])
+     #       print (rad_mean)
+    #return (rad_mean)
+#rad_mean = radical(elements)
