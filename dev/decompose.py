b378e13441ab851e549fd5a10a460fd7d7dbbc7f
KatyaKalache <katya@kalache.fr>
2017-11-01 01:47:08 +0000

    Done splitting the function and handle returns properly. Added comments for each method.

--- dev/decompose.py
+++ dev/decompose.py
@@ -12,42 +12,59 @@ if (len(argv) < 2):
     print ("Please enter at least one character")
     exit()
 
+
 def decom():
+# Finds the in database and returns the object with its dictionary
     argument = argv[1]
     counter = 0
+    # Validates that Chinese character passed
     valid = re.findall(r'[\u4e00-\u9fff]+', argument)
-    dictionary = {}
+    res = []
     if valid == []:
         print ("Please enter a Chinese character")
-    while (counter < 3):
+        exit()
+    while (counter < len(argv)):
         for i in valid:
             for j in i:
                 search_char = (db.characters.find({"character": j}))
-                for decomp in search_char:
-                    definition = (decomp["definition"])
-                    print (definition)
+                res.append(search_char)
+            return (res)
         counter += 1
-#    return (search_char)
-search_char=decom()
-#search_char=decom()
-#print (search_char)
-#def translate(search_char):
- #   for decomp in search_char:
+res = decom()
 
 
-  #      elements = (decomp["decomposition"])
-   #     character = (decomp["character"])
-    #    definition = (decomp["definition"])
-     #   res = (character + " " + definition + '\n' + elements)
-      #  print (elements)
-   # return (elements)
-#elements = translate(search_char)
+def translate(res):
+    final = ""
+    counter = 0
+    while (counter < len(argv)):
+        for obj in res:
+            for decomp in obj:
+                character = (decomp["character"])
+                definition = (decomp["definition"])
+                char_def = character + " " + definition
+                final += char_def + "\n"
+        counter += 1
+    return (final)
+final = translate(res)
+print (final)
+res = decom()
 
-#def radical(elements):
- #   for each in elements:
-  #      search_radical = db.radicals.find({"radical": each})
-   #     for radical in search_radical:
-    #        rad_mean = (each + radical["meaning"])
-     #       print (rad_mean)
-    #return (rad_mean)
-#rad_mean = radical(elements)
+
+def ele(res):
+# Finds elements from one db and their meanings from another
+    counter = 0
+    rad_mean = ""
+    while (counter < len(argv)):
+        for obj in res:
+             for decomp in obj:
+                 elements = (decomp["decomposition"])
+                 print(elements)
+                 for each in elements:
+                     search_radical = db.radicals.find({"radical": each})
+                     for radical in search_radical:
+                         r = (each + " " + radical["meaning"] + "\n")
+                         rad_mean += r
+        counter += 1
+    return (rad_mean)
+rad_mean = ele(res)
+print (rad_mean)
