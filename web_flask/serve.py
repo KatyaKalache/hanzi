bd23e0e2f26584aee7e954c2f8b757175c7494da
KatyaKalache <katya@kalache.fr>
2017-11-01 20:52:30 +0000

    Saving the initial short version that returns only character meaning when called

--- web_flask/serve.py
+++ web_flask/serve.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 # starts a Flask web application
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from dev import test
+from pymongo import MongoClient
+import re
+
+decom = test.decom
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def decomp():
-    return render_template('index.html')
+@app.route('/', strict_slashes=False, methods=['GET'])
+def display():
+    return render_template('text.html')
+
+@app.route('/hello', methods=['GET', 'POST'], strict_slashes=False)
+def char():
+    argument = request.form['first_name']
+    word_associations = decom(argument)
+    return word_associations
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
