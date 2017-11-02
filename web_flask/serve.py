12cb8e4e4e987ee435fe17c51838cd8276054056
KatyaKalache <katya@kalache.fr>
2017-11-02 04:28:20 +0000

    updated frontend

--- web_flask/serve.py
+++ web_flask/serve.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 # starts a Flask web application
 from flask import Flask, render_template, request
-from dev import test
-from pymongo import MongoClient
-import re
+from dev import dec
 
-decom = test.decom
+decom = dec.decom
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False, methods=['GET'])
 def display():
-    return render_template('text.html')
+    return render_template('index.html')
 
-@app.route('/hello', methods=['GET', 'POST'], strict_slashes=False)
+@app.route("/character", methods=['GET', 'POST'], strict_slashes=False)
 def char():
-    argument = request.form['first_name']
+    argument = request.form["character"]
+    if len(argument) < 1:
+        return render_template('index.html')
     word_associations = decom(argument)
     return word_associations
 
