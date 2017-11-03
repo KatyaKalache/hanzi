53ec4b4a53eb0469d6b360004423b486d9c0c9e8
KatyaKalache <katya@kalache.fr>
2017-11-03 20:32:25 +0000

    Saving before splitting the function to return radicals fro each character in the list separately

--- dev/decompose.py
+++ dev/decompose.py
@@ -5,68 +5,60 @@ from sys import argv
 from pymongo import MongoClient
 import re
 
-if __name__ == '__main__':
-    client = MongoClient()
-    db = client.dictionary
-
-    if (len(argv) < 2):
-        print ("Please enter at least one character")
-        exit()
+client = MongoClient()
+db = client.dictionary
 
 
-    def decom():
-    # Finds the in database and returns the object with its dictionary
-        argument = argv[1]
-        counter = 0
-        # Validates that Chinese character passed
-        valid = re.findall(r'[\u4e00-\u9fff]+', argument)
-        res = []
-        if valid == []:
-            print ("Please enter a Chinese character")
-            exit()
-        while (counter < len(argv)):
-            for i in valid:
-                for j in i:
-                    search_char = (db.characters.find({"character": j}))
-                    res.append(search_char)
-                return (res)
-            counter += 1
-    res = decom()
-
-
-    def translate(res):
-    # Returning character requested and it's translation
-        final = ""
-        counter = 0
-        while (counter < len(argv)):
-            for obj in res:
-                for decomp in obj:
+def decom(argument):
+# Finds the in database and returns the object with its dictionary
+    counter = 0
+    seen = []
+    final = []
+    # Validates that Chinese character passed
+    valid = re.findall(r'[\u4e00-\u9fff]+', argument)
+    if valid == []:
+        return ("Please enter a Chinese character")
+    while (counter < len(argument)):
+        for i in valid:
+            for j in i:
+                search_char = (db.characters.find({"character": j}))
+                for decomp in search_char:
                     character = (decomp["character"])
                     definition = (decomp["definition"])
-                    char_def = character + " " + definition
-                    final += char_def + "\n"
-            counter += 1
+                    char_def = character + "  " + definition
+                seen.append(char_def)
+                for character in seen:
+                    if character not in final:
+                        final.append(char_def)
+                counter += 1
         return (final)
-    final = translate(res)
-    print (final)
-    res = decom()
 
 
-    def ele(res):
-    # Finds elements from one db and their meanings from another
-        counter = 0
-        rad_mean = ""
-        while (counter < len(argv)):
-            for obj in res:
-                for decomp in obj:
+def ele(argument):
+# Finds elements from one db and their meanings from another
+    counter = 0
+    rad_mean = []
+    valid = re.findall(r'[\u4e00-\u9fff]+', argument)
+    seen = []
+    if valid == []:
+        return ("Please enter a Chinese character")
+
+    while (counter < len(argument)):
+        for i in valid:
+            for j in i:
+                search_char = (db.characters.find({"character": j}))
+                for decomp in search_char:
                     elements = (decomp["decomposition"])
-                    print(elements)
                     for each in elements:
                         search_radical = db.radicals.find({"radical": each})
                         for radical in search_radical:
-                            r = (each + " " + radical["meaning"] + "\n")
-                            rad_mean += r
-            counter += 1
+                            r = (each + " " + radical["meaning"])
+                            seen.append(r)
+                            for i in seen:
+                                if i not in rad_mean:
+                                    rad_mean.append(i)
+                counter += 1
         return (rad_mean)
-    rad_mean = ele(res)
-    print (rad_mean)
+
+if __name__ == '__main__':
+    pass
