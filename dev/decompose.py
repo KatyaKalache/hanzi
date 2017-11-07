6863e42c239f45a94f084354c71d5da2ba1bff08
KatyaKalache <katya@kalache.fr>
2017-11-07 19:18:49 +0000

    Centered text in result field, rm unnecessary sections from html

--- dev/decompose.py
+++ dev/decompose.py
@@ -8,6 +8,23 @@ import re
 client = MongoClient()
 db = client.dictionary
 
+def character_print(argument):
+# Finds the in database and returns the object with its dictionary
+    counter = 0
+    seen = []
+    final = []
+    # Validates that Chinese character passed
+    valid = re.findall(r'[\u4e00-\u9fff]+', argument)
+    if valid == []:
+        return ("Please enter a Chinese character")
+    while (counter < len(argument)):
+        for i in valid:
+            for j in i:
+                search_char = (db.characters.find({"character": j}))
+                for decomp in search_char:
+                    character = (decomp["character"])
+                    final.append(character)
+            return (final)
 
 def decom(argument):
 # Finds the in database and returns the object with its dictionary
@@ -25,13 +42,13 @@ def decom(argument):
                 for decomp in search_char:
                     character = (decomp["character"])
                     definition = (decomp["definition"])
-                    char_def = character + "  " + definition
-                seen.append(char_def)
-                for character in seen:
-                    if character not in final:
-                        final.append(char_def)
+                    cd = character + "  " + definition
+                seen.append(cd)
+                for charac in seen:
+                    if charac not in final:
+                        final.append(cd)
                 counter += 1
-        return (final)
+            return (final)
 
 
 def ele(argument):
@@ -40,6 +57,9 @@ def ele(argument):
     rad_mean = []
     valid = re.findall(r'[\u4e00-\u9fff]+', argument)
     seen = []
+    seen_rad = []
+    cr = []
+    elem = []
     if valid == []:
         return ("Please enter a Chinese character")
 
@@ -48,17 +68,24 @@ def ele(argument):
             for j in i:
                 search_char = (db.characters.find({"character": j}))
                 for decomp in search_char:
+                    character = (decomp["character"])
                     elements = (decomp["decomposition"])
+                    etymology = (decomp["etymology"])
+                    char_el = character + "  =>   " + "  " + elements[1:]
+                    seen.append(char_el)
+                    for i in seen:
+                        if i not in cr:
+                            cr.append(i)
                     for each in elements:
                         search_radical = db.radicals.find({"radical": each})
                         for radical in search_radical:
                             r = (each + " " + radical["meaning"])
-                            seen.append(r)
-                            for i in seen:
-                                if i not in rad_mean:
-                                    rad_mean.append(i)
+                            seen_rad.append(r)
+                            for j in seen_rad:
+                                if j not in cr:
+                                    cr.append(j)
                 counter += 1
-        return (rad_mean)
+            return (cr)
 
 if __name__ == '__main__':
     pass
