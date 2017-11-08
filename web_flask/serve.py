22ec71eb2fda08bd26262e78bd9f292563fbd986
KatyaKalache <katya@kalache.fr>
2017-11-08 01:29:26 +0000

    updating with the newiest version

--- web_flask/serve.py
+++ web_flask/serve.py
@@ -1,22 +1,37 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 # starts a Flask web application
 from flask import Flask, render_template, request
-from dev import dec
+from dev import decompose
+from dev import etymology
+import re
 
-decom = dec.decom
+
+decom = decompose.decom
+ele = decompose.ele
+etymol = etymology.etymol
+character_print = decompose.character_print
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False, methods=['GET'])
 def display():
-    return render_template('text.html')
+    return render_template('index.html')
 
-@app.route("/", methods=['GET', 'POST'], strict_slashes=False)
+@app.route('/', methods=['GET', 'POST'], strict_slashes=False)
 def char():
     argument = request.form["character"]
+    valid = re.findall(r'[\u4e00-\u9fff]+', argument)
     if len(argument) < 1:
-        return render_template('text.html')
+        message_no_argument = "Please enter at least one character"
+        return render_template('index.html',  **locals())
+    if valid == []:
+        message_not_valid = "Please enter a Chinese character"
+        return render_template('index.html',  **locals())
+    character = character_print(argument)
     word_associations = decom(argument)
-    return render_template('text.html', **locals())
+    radicals = ele(argument)
+    etymology = etymol(argument)
+
+    return render_template('index.html', **locals())
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
